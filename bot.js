c8dbbea4e2d9ee1278843e7803c67be60649b916
corecode2026 <hk983480@gmail.com>
2026-08-08 14:28:30 +0300

    Remove unnecessary http module import

--- bot.js
+++ bot.js
@@ -284,7 +284,6 @@ function markPresence(username) {
 }
 
 // ══ Keep-Alive ══
-const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Bot is alive!');
